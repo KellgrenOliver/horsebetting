01ece49c339bbf07a583dcdeef6e0b40101c757f
Oliver Kellgren <oliver-fotboll@live.se>
2022-04-20 10:52:54 +0200

    added typewriter effect

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import LogInPage from "./LogInPage";
 import Header from "../components/Header";
 import InfoText from "../components/HomePageText";
 import HomePageGamesButton from "../components/HomePageGamesButton";
+import Typewriter from "typewriter-effect";
 
 const HomePage = () => {
   const { currentUser } = useContext(AuthContext);
@@ -13,6 +14,22 @@ const HomePage = () => {
       {currentUser ? (
         <>
           <Header title={"HORSE BETTING"} />
+          <Typewriter
+            options={{
+              autoStart: true,
+              loop: true,
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .pauseFor(500)
+                .typeString("Underrubrik 1")
+                .pauseFor(2000)
+                .deleteAll()
+                .typeString("Underrubrik 2")
+                .pauseFor(2000)
+                .start();
+            }}
+          />
           <InfoText />
           <HomePageGamesButton />
         </>
